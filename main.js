d1571a3dbfdf4e9df28e833454c43abc58d008a3
Youstina Emad Ghoprial <totatenaemad@gmail.com>
2024-06-29 07:48:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let cart = document.querySelector('.cart');
+document.querySelector('#cart-icon').onclick = () =>{
+    cart.classList.toggle('active');
+    search.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
+let user = document.querySelector('.user');
+document.querySelector('#user-icon').onclick = () =>{
+    user.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+}
+
+window.onscroll = () => {
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
